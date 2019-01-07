d44b607d8777e3efc2d4b3d41c6e9ff8262b59ed
dayeon.lee <yeonylee94@gmail.com>
2019-01-08 02:00:52 +0900

    ENH: Create blockchain.js that builds Blockchain

---
+++ blockchain.js
@@ -0,0 +1,16 @@
+const Block = require("./block");
+
+class BlockChain {
+  constructor() {
+    this.chain = [Block.genesis()]; //give genesis block
+  }
+
+  addBlock(data) {
+    //need lastBlock as input
+    const lastBlock = this.chain[this.chain.length - 1]; //grab one that is currently at the end of the chain
+    const block = Block.mineBlock(lastBlock, data);
+    this.chain.push(block);
+
+    return block;
+  }
+}
